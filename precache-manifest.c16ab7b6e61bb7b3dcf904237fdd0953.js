d1c486035f07af4b4209e17599a6b05970368901
kkadapa <kkadapa@northwell.edu>
2020-06-09 09:36:24 -0400

    Updates

---
+++ precache-manifest.c16ab7b6e61bb7b3dcf904237fdd0953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972102f625a8c395b798dfea8cd31fb5",
+    "url": "/latranstandgen/index.html"
+  },
+  {
+    "revision": "3410e2b88cbdc7b6c0a3",
+    "url": "/latranstandgen/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d0466abe01e2c370480d",
+    "url": "/latranstandgen/static/js/2.a2b2f0b8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/latranstandgen/static/js/2.a2b2f0b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3410e2b88cbdc7b6c0a3",
+    "url": "/latranstandgen/static/js/main.162453a2.chunk.js"
+  },
+  {
+    "revision": "73e25bacc3ad1da39aeb",
+    "url": "/latranstandgen/static/js/runtime-main.5f3383e9.js"
+  }
+]);
